15ab34c4acd13ecf63c3972613c8a3eefd97bc3d
iakovleven <3259713@gmail.com>
2023-10-11 23:11:47 +0300

    prjct

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import "./App.css";
+
+import React from 'react';
+import { useState } from "react";
+
+const App = () => {
+
+    const [count, setCount] = useState(0)
+
+    const handleDec = () => {
+        setCount(count - 1)
+    }
+
+    const handleInc = () => {
+        setCount(count + 1);
+    };
+
+
+
+    return (
+        <div className="counter">
+            <div className="count">
+                <div>{count}</div>
+                <button className="btn_dec" onClick={handleDec}>-</button>
+                <button className="btn_inc" onClick={handleInc}>+</button>
+            </div>
+        </div>
+    );
+};
+
+export default App;
